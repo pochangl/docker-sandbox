afc717f3f67f66c5ea259aaedc06c892f1e6827a
Pochang Lee <pochangl@usc.edu>
2019-10-05 07:30:39 +0800

    basic vue project setup

---
+++ web/src/views/Home.vue
@@ -0,0 +1,11 @@
+<template lang="pug">
+  div(class="home")
+    p This is a Home page
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class Home extends Vue {}
+</script>
