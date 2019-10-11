4ee1dd7b33b865be43f30b65443f61304ebc9480
Pochang Lee <pochangl@usc.edu>
2019-10-11 23:07:23 +0800

    add csrftoken token in request

--- web/src/http.ts
+++ web/src/http.ts
@@ -1,6 +1,23 @@
 import Vue from 'vue'
-import { Http } from 'vue-resource/types/vue_resource'
+import { Http, HttpInterceptor } from 'vue-resource/types/vue_resource'
 import VueResource from 'vue-resource'
 
 Vue.use(VueResource)
-export default (Vue as any).http as Http
+const http = (Vue as any).http as Http
+export default http
+
+function getCookie (cookieKey: string) {
+  const cookieName = `${cookieKey}=`
+  const cookieArray = document.cookie.split(';').map(w => w.trim())
+
+  for (const cookie of cookieArray) {
+    if (cookie.indexOf(cookieName) == 0) {
+      return cookie.substring(cookieName.length, cookie.length);
+    }
+  }
+}
+
+function setCSRF (request: any) {
+  request.headers.set('X-CSRFToken', getCookie('csrftoken'))
+}
+http.interceptors.push(setCSRF as any)
