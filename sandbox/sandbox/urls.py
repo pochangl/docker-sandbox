d6abc2571c5a62ec8c5009f4031ce148f25aca2d
Pochang Lee <pochangl@usc.edu>
2019-10-11 23:08:43 +0800

    fixed url

--- sandbox/sandbox/urls.py
+++ sandbox/sandbox/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(('problem.urls', 'problem'), namespace='problem')),
+    path('api/problem/', include(('problem.urls', 'problem'), namespace='problem')),
 ]
