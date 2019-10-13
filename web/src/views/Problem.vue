da418b9496ccaab16a65356a1fa922a447128afe
Pochang Lee <pochangl@usc.edu>
2019-10-13 19:57:50 +0800

    enhance page

--- web/src/views/Problem.vue
+++ web/src/views/Problem.vue
@@ -1,16 +1,34 @@
 <template lang="pug">
   v-container(style="height: 90vh" fluid)
     v-layout(row wrap fill-height)
-      v-flex.flex-grow-0.pa-4
-        p {{ problemModel.title }}
-        p {{ problemModel.description }}
-        template(v-if="submission")
-          pre.green--text {{ submission.stdout }}
-          pre.red--text(v-if="submission.stderr") {{ submission.stderr }}
-      v-flex
+      v-flex.description.flex-grow-0.pa-4
+        v-card(flat color="transparent")
+          v-card-title.white
+            h1.font-weight-bold.display-1 {{ problemModel.title }}
+
+          v-tabs(
+            v-model="tab"
+            color="grey"
+            grow
+          )
+            v-tab(href="#description") 題目描述
+            v-tab(href="#stdout" :disabled="!submission.stdout") 輸出結果
+            v-tab(href="#stderr" :disabled="!submission.stderr") 錯誤結果
+          v-tabs-items.content(v-model="tab")
+            v-tab-item(value="description")
+              v-card(flat)
+                v-card-text {{ problemModel.description }}
+            v-tab-item(value="stdout")
+              v-card(flat)
+                v-card-text
+                  pre.green--text {{ submission.stdout }}
+            v-tab-item(value="stderr")
+              v-card(flat)
+                v-card-text
+                  pre.red--text {{ submission.stderr }}
+      v-flex.pt-4.pr-4
         code-editor(@submit="submit")
 </template>
-
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
 import CodeEditor from '@/components/CodeEditor.vue'
@@ -33,10 +51,12 @@ export default class ProblemPage extends Vue {
   get problem () {
     return this.$route.params.problem
   }
-  submission: Submission | null = null
+  submission: Submission = new Submission()
 
   @ProblemTransformer('problem')
   problemModel: Problem = new Problem()
+
+  tab: string = 'description'
 
   async submit(code: string) {
     const submission = new Submission()
@@ -44,6 +64,25 @@ export default class ProblemPage extends Vue {
     submission.problem = this.problemModel.id
     await submission.create()
     this.submission = submission
+    this.focusTab()
+  }
+
+  focusTab () {
+    if (this.submission.stderr) {
+      this.tab = 'stderr'
+    } else if (this.submission.stdout) {
+      this.tab = 'stdout'
+    } else {
+      this.tab = 'description'
+    }
   }
 }
 </script>
+<style lang="sass" scoped>
+  .description
+    min-width: 500px
+  .content
+    background-color: transparent
+    .v-card
+      background-color: transparent
+</style>
