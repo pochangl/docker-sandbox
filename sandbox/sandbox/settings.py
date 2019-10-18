36f96932ac4ca62e9f82b2a00dbec0b47dc2ac1b
Pochang Lee <pochangl@usc.edu>
2019-10-19 07:54:31 +0800

    channel setup

--- sandbox/sandbox/settings.py
+++ sandbox/sandbox/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'problem',
+    'channels'
 ]
 
 MIDDLEWARE = [
@@ -69,6 +70,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'sandbox.wsgi.application'
+ASGI_APPLICATION = "sandbox.routing.application"
 
 
 # Database
