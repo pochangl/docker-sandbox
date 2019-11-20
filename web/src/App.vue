1e853f649f54b70f151b6cf7eae06e8fd61971b7
Pochang Lee <pochangl@usc.edu>
2019-11-20 20:48:04 +0800

    add navbar

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,5 +1,14 @@
 <template lang="pug">
   div(id="app")
     v-app
+      navbar
       router-view
 </template>
+<script lang="ts">
+import Navbar from '@/components/Navbar.vue'
+export default {
+  components: {
+    Navbar
+  }
+}
+</script>
---
+++ web/src/components/Navbar.vue
@@ -0,0 +1,24 @@
+<template lang="pug" functional>
+  v-toolbar.flex-grow-0
+    v-toolbar-items
+      v-btn(
+        exact-active-class="hide"
+        exact
+        text
+        router
+        :to="{name: 'home'}"
+      ) Home
+    v-spacer
+    v-toolbar-items
+      v-btn(
+        router
+        href="https://github.com/pochangl/docker-sandbox"
+        target="_blank"
+        text
+      )
+        i.fab.fa-github.fa-2x
+</template>
+<style lang="sass" scoped>
+.hide
+  display: none
+</style>
