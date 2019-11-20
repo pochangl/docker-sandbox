522cc02c0d90f622643231afff2904692efe955f
Pochang Lee <pochangl@usc.edu>
2019-11-20 19:45:41 +0800

    support full html

--- web/src/components/problem/output/TextHtml.vue
+++ web/src/components/problem/output/TextHtml.vue
@@ -1,7 +1,43 @@
-<template lang="pug" functional>
-  v-container
-    div(v-html="props.value")
+<template lang="pug">
+  v-container.pa-0.ma-0
+    iframe(v-if="isFullHTML" ref="iframe") {{ html }}
+    div(v-else v-html="html")
 </template>
 <script lang="ts">
-export default {}
+import { Vue, Component, Prop, Watch } from 'vue-property-decorator'
+
+@Component
+export default class TextHtml extends Vue {
+  @Prop({ type: String, required: true })
+  value: string
+
+  get isFullHTML() {
+    return this.html.search('<html') >= 0
+  }
+
+  get html() {
+    return this.value
+  }
+
+  @Watch('value', { immediate: true })
+  async onValue () {
+    await this.$nextTick()
+    this.updateResult()
+  }
+
+  updateResult () {
+    if (this.isFullHTML) {
+      const iframe: { contentWindow: Window } = this.$refs.iframe as any
+      iframe.contentWindow.document.open()
+      iframe.contentWindow.document.write(this.html)
+      iframe.contentWindow.document.close()
+    }
+  }
+}
 </script>
+<style lang="sass" scoped>
+iframe
+  border: none
+  width: 100%
+  height 100%
+</style>
