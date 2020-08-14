e86054bf092611b27ad0c7df9221a23b349e0ce4
Pochang <pochanglee2008@gmail.com>
2020-08-14 06:21:31 +0000

    modify: fix deprecation warning

--- sandbox/sandbox/routing.py
+++ sandbox/sandbox/routing.py
@@ -1,12 +1,12 @@
 from channels.routing import ProtocolTypeRouter, URLRouter
-from django.conf.urls import url
+from django.urls import path
 from problem import consumers as pconsumers
 from worker import consumers as wconsumers
 
 application = ProtocolTypeRouter({
     'websocket': URLRouter([
-        url(r'^ws/problem/submission/$', pconsumers.SubmissionConsumer),
-        url(r'^ws/worker/$', wconsumers.SandboxWorkerConsumer),
+        path('ws/problem/submission/', pconsumers.SubmissionConsumer),
+        path('ws/worker/', wconsumers.SandboxWorkerConsumer),
     ])
     # Empty for now (http->django views is added by default)
 })
